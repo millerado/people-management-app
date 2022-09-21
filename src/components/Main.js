914ab45ac3de843ec2d219c2bbb816dc1b04583c
Seth Miller <millerado@gmail.com>
2022-09-21 07:32:16 -0600

    render index and show components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,14 @@
+import Index from '../pages/Index';
+import Show from '../pages/Show';
+
 function Main(props) {
-  return <h1>The Main Componenet</h1>
+  return (
+    <>
+      <h1>The Main Componenet</h1>
+      <Index />
+      <Show />
+    </>
+  );
 }
 
 export default Main;
