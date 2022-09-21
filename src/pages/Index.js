eb3836e7842ce703fbc52b22d6d607db3c8028bc
Seth Miller <millerado@gmail.com>
2022-09-21 07:31:58 -0600

    create basic component

---
+++ src/pages/Index.js
@@ -0,0 +1,5 @@
+function Index(props) {
+  return <h1>This is the Index Page Component</h1>;
+}
+
+export default Index;
---
+++ src/pages/Show.js
@@ -0,0 +1,5 @@
+function Show(props) {
+  return <h1>I am the Show Page Componenet</h1>;
+}
+
+export default Show;
